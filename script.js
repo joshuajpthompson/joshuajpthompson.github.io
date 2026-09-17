5b2972f481a1714dfe566074facdd8d2ef74f1a7
joshuajpthompson <joshuajp.thompson@gmail.com>
2026-09-17 17:28:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const addResearchFigure = (card, src, alt, extraClass = '') => {
 
 addResearchFigure(
   researchCards[1],
-  'assets/research-hybrid-materials.webp',
+  'assets/fig24website.png',
   'Molecular structure illustrating a low-dimensional hybrid material.',
   'research-figure-hybrid'
 );
